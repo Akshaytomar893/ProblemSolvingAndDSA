9886896b75b9c19a1574b38a105bb6ea937f1449
Akshay Tomar <54436313+Akshaytomar893@users.noreply.github.com>
2026-01-29 03:56:04 +0530

    Time: 1 ms (88.08%) | Memory: 44.1 MB (84.64%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
-    private void getSubsets(int nums[], List<Integer> subset, int index, List<List<Integer>> result){
-        if(index >= nums.length){
-            result.add(new ArrayList<>(subset));
-            return;
-        }
-         getSubsets(nums, subset, index + 1, result);
-        int ele = nums[index];
-        subset.add(ele);
-        getSubsets(nums, subset, index + 1, result);
-         subset.remove(subset.size() - 1);
-    }
+    //Recursive method:
+
+    // private void getSubsets(int nums[], List<Integer> subset, int index, List<List<Integer>> result) {
+    //     if (index >= nums.length) {
+    //         result.add(new ArrayList<>(subset));
+    //         return;
+    //     }
+    //     getSubsets(nums, subset, index + 1, result);
+    //     int ele = nums[index];
+    //     subset.add(ele);
+    //     getSubsets(nums, subset, index + 1, result);
+    //     subset.remove(subset.size() - 1);
+    // }
+
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     List<Integer> subset = new ArrayList<>();
+    //     getSubsets(nums, subset, 0, result);
+    //     return result;
+    // }
 
     public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int subsetCount = 1 << n;
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
-        getSubsets(nums, subset, 0, result);
+        for (int i = 0; i < subsetCount; i++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
+        }
         return result;
     }
 }
